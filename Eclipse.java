a4197f3740de18631cbbfb82e86a6d019891be6e
setsero <orestesportela@gmail.com>
2017-08-10 20:18:18 -0700

    initial commit

---
+++ Eclipse.java
@@ -0,0 +1,38 @@
+//Orestes Portela
+public class Eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("/////////");
+		System.out.println("|| \" \" ||");
+		System.out.println("||  _  ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\");
+		
+		System.out.println();
+		
+		System.out.println("  \\/");
+		System.out.println(" \\\\//");
+		System.out.println("\\\\\\///");
+		System.out.println("///\\\\\\");
+		System.out.println(" //\\\\");
+		System.out.println("  /\\");
+		
+		System.out.println();
+		
+		System.out.println("  /\\");
+		System.out.println(" //\\\\");
+		System.out.println("///\\\\\\");
+		System.out.println("\\\\\\///");
+		System.out.println(" \\\\//");
+		System.out.println("  \\/");
+		
+		System.out.println();
+		
+		System.out.println("  /~~~~~~~~\\");
+		System.out.println(" /_ _ _ _ _ \\");
+		System.out.println("{ _\"_\'_\"_\'_\" }");
+		System.out.println(" \\          /");
+		System.out.println("  \\~~~~~~~~/");
+	}
+
+}
---
+++ Escape.java
@@ -0,0 +1,39 @@
+
+public class Escape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("/////////");
+		System.out.println("|| \" \" ||");
+		System.out.println("||  _  ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\");
+		
+		System.out.println();
+		
+		System.out.println("  \\/");
+		System.out.println(" \\\\//");
+		System.out.println("\\\\\\///");
+		System.out.println("///\\\\\\");
+		System.out.println(" //\\\\");
+		System.out.println("  /\\");
+		
+		System.out.println();
+		
+		System.out.println("  /\\");
+		System.out.println(" //\\\\");
+		System.out.println("///\\\\\\");
+		System.out.println("\\\\\\///");
+		System.out.println(" \\\\//");
+		System.out.println("  \\/");
+		
+		System.out.println();
+		
+		System.out.println("  /~~~~~~~~\\");
+		System.out.println(" /_ _ _ _ _ \\");
+		System.out.println("{ _\"_\'_\"_\'_\" }");
+		System.out.println(" \\          /");
+		System.out.println("  \\~~~~~~~~/");
+	}
+
+}
